70a57256030ad0cb289821223206cb3d33334b4b
BeautyKim <kima2012114@gmail.com>
2022-05-31 17:38:31 +0900

    타입과 인터페이스

---
+++ components/board/Article.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { IArticle } from "@/types"
+
+type Props = {
+    article: IArticle,
+    deletePost: (id: number) => void
+}
+
+const Article: React.FC<Props> = ({article, deletePost}) => {
+    return(
+        <>
+            <h1>게시글</h1>
+            <h1>글번호: {article.artId}</h1>
+            <h1>제목: {article.title}</h1>
+            <h1>내용: {article.content}</h1>
+            <button onClick={()=> deletePost(article.artId)}>삭제</button>
+        </>
+    )
+}
+export default Article
---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      
+        <Html>
+        <Head>
+          <title>BeautyKim</title>
+          <meta charSet="utf-8"></meta>
+        </Head>
+          <body>
+            <Main />
+            <NextScript />
+          </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
